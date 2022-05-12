0e68c554bd90ec845bbb6911c6619fb724e2183d
Ortal Davidov <ord4220698@gmail.com>
2022-05-12 20:37:38 +0300

    more validations to rating

--- Ratings2_2/Ratings2_2/Models/RatingObj.cs
+++ Ratings2_2/Ratings2_2/Models/RatingObj.cs
@@ -5,9 +5,11 @@ namespace Ratings2_2.Models
     public class RatingObj
     {
         public int Id { set; get; }
+        
         [Required(ErrorMessage = "Name is required ")]
         public string Name { set; get; }
 
+        [Range(1,5)]
         public int Rate { set; get; }
         public string Description { set; get; }
         public string Date { set; get; }
